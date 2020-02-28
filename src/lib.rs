6730850a32680c7a17c7cf55bf9fe95fc20869dd
elenadame <adamelena131@gmail.com>
2020-02-28 08:40:39 -0800

    Added Tokenize_syntax method for all syntax. Finished tokenize_bool for boolean expression Tokens

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod token;
+use std::iter::Peekable;
 
 pub mod tokenizer {
     pub use crate::token::Token;
@@ -12,13 +13,52 @@ pub mod tokenizer {
         None
     }
     fn tokenize_bool(word: &str) -> Option<Token> {
-        None
+        match word {
+            "true" => Some(Token::Bool(true)),
+            "false" => Some(Token::Bool(false)),
+            _ => None
+        }
     }
+
+
     fn tokenize_str(word: &str) -> Option<Token> {
         None
     }
+
+
     fn tokenize_var_or_keyword(word: &str) -> Option<Token> {
         None
+    }
+
+    fn tokenize_syntax(word: &str) -> Option<Token>{
+        match work{
+            "{" => Some(Token::LeftCurly),
+            "}" => Some(Token::RightCurly),
+            "(" => Some(Token::LeftParen),
+            ")" => Some(Token::RightParen),
+            "[" => Some(Token::LeftBrace),
+            "]" => Some(Token::RightBrace),
+            "." => Some(Token::Dot),
+            "," => Some(Token::Comma),
+            ";" => Some(Token::Semicolon),
+            ":" => Some(Token::Colon),
+            "," => Some(Token::Comma),
+            "+" => Some(Token::Plus),
+            "-" => Some(Token::Minus),
+            "/" => Some(Token::Divide),
+            "*"" => Some(Token::Multiply),
+            "%" => Some(Token::Modulo),
+            "&&" => Some(Token::And),
+            "||" => Some(Token::Or),
+            "!" => Some(Token::Not),
+            ">" => Some(Token::GreaterThan),
+            "<" => Some(Token::LessThan),
+            ">=" => Some(Token::GreaterEqual),
+            "<=" => Some(Token::LessEqual),
+            "==" => Some(Token::Equal),
+            "!=" => Some(Token::Or),
+             _ => None
+        }
     }
 
     // takes some iterator and modifies it
